ec26099a59e74bda856f005676a771ef00f93a1a
Freddy Asiani <135480796+Frederic-Asiani@users.noreply.github.com>
2026-05-07 03:38:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
 const express = require("express");
-const fetch = require("node-fetch");
-const cors = require("cors");
-require("dotenv").config();
+const path = require("path");
 
 const app = express();
-app.use(cors());
 
-const API_KEY = process.env.GOOGLE_API_KEY;
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/api/lighthouse", async (req, res) => {
-    const url = req.query.url;
-
-    try {
-        const api = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${API_KEY}`;
-
-        const response = await fetch(api);
-        const data = await response.json();
-
-        const categories = data.lighthouseResult.categories;
-
-        res.json({
-            performance: Math.round(categories.performance.score * 100),
-            seo: Math.round(categories.seo.score * 100),
-            mobile: Math.round(categories["best-practices"].score * 100),
-            conseil: "Optimisez votre site pour gagner plus de clients."
-        });
-
-    } catch (error) {
-        res.status(500).json({ error: "Erreur API" });
-    }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(3000, () => console.log("API running"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
